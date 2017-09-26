793d6f40f33645691e17eaaf2ccb48ff7ca19dfe
master <deveron-reniers@hotmail.com>
2017-09-26 08:16:37 +0200

    demo order kiosk

--- resources/views/kiosk/order/index.blade.php
+++ resources/views/kiosk/order/index.blade.php
@@ -2,88 +2,267 @@
 
 @section('content')
     <div class="container">
-        <div class="col-lg-3" style="margin-top: 500px;">
-            <div class="panel panel-content" id="filter" >
-                <div class="panel-body">
-                    <div>filter</div>
-                    <div>
-                        @foreach($menu as $i)
-                            <a class="col-md-12 btn btn-default" style="margin-bottom: 3px;">{{$i->naam}}</a>
-                        @endforeach
+        {{--<button id="click">Click me</button>--}}
+
+        <div class="col-sm-3" style="margin-top: 0px;">
+            <div class="affix" style="width: 250px;">
+                <div class="panel panel-content" id="">
+                    <div class="panel-body">
+                        {{--<div>filter</div>--}}
+                        <div>
+                            <ul>
+                                <h2 style=" font: 400 40px/1.5 Helvetica, Verdana, sans-serif;">Menu</h2>
+                                @foreach($menu as $i)
+                                    <li >
+                                        <a class="" style="margin-bottom: 3px;">{{$i->naam}}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-9 col-sm-9">
-            @foreach($product as $i)
-                <div class="panel panel-content">
-                    <div class="panel-body">
-                        <h3>asdasdsa</h3>
-asdasd
+            @foreach($menu as $i)
 
-                        {{--{{dd($i->productIngredient())}}--}}
-                        @foreach($i->productIngredient as $ingr)
-                            Gemaakt met: {{$ingr->ingredient->ingredient}},
-                        @endforeach
+                <div class="row">
+                    <div class="col-sm-12">
 
+                        <div class="panel panel-body menu-cate" style="margin-bottom: -30px; border-radius: 5px; border: none;">
+                            <div class="panel-body">
+                                {{--<img src="https://dummyimage.com/1000x200/000/fff">--}}
+                                <h1 style="color: #FFFFFF">{{$i->naam}}</h1>
+                                {{--<h1>{{$i->naam}}</h1>--}}
+                                {{--<hr>--}}
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+
+                @foreach($i->menuProduct as $m)
+                    <div class="col-sm-12">
+                        <div class="panel panel-content" style="background: #FFFFFF;">
+                            <div class="panel-body">
+                                <div class="col-md-3">
+                                    <img src="/image/soep/{{rand(1,5)}}.jpg" class="img-responsive center-block"  style="height: 130px;">
+                                </div>
+                                <div class="col-md-6">
+                                    <h3>{{$m->product->naam}}</h3>
+{{--                                    <h3>{{$m->product->menuProduct->menu->naam}}</h3>--}}
+                                    {{--{{dd($i->productIngredient())}}--}}
+                                    @foreach($m->product->productIngredient as $ingr)
+                                        Gemaakt met: {{$ingr->ingredient->ingredient}},
+                                    @endforeach
+                                </div>
+                                <div class="col-md-3">
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <a href="" class="btn btn-primary pull-right">
+                                        <span>
+                                            <i class="fa fa-eur" aria-hidden="true"></i>
+                                            {{$m->product->prijs}}
+                                        </span> |
+                                        <i class="fa fa-plus" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="panel panel-content" style="background: #FFFFFF;">
+                            <div class="panel-body">
+                                <div class="col-md-3">
+                                    <img src="/image/soep/{{rand(1,5)}}.jpg" class="img-responsive  center-block" style="height: 130px;">
+                                </div>
+                                <div class="col-md-6">
+                                    <h3>{{$m->product->naam}}</h3>
+{{--                                    <h3>{{$m->product->menuProduct->menu->naam}}</h3>--}}
+                                    {{--{{dd($i->productIngredient())}}--}}
+                                    @foreach($m->product->productIngredient as $ingr)
+                                        Gemaakt met: {{$ingr->ingredient->ingredient}},
+                                    @endforeach
+                                </div>
+                                <div class="col-md-3">
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <a href="" class="btn btn-primary pull-right">
+                                        <span>
+                                            <i class="fa fa-eur" aria-hidden="true"></i>
+                                            {{$m->product->prijs}}
+                                        </span> |
+                                        <i class="fa fa-plus" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             @endforeach
-                @foreach($product as $i)
-                    <div class="panel panel-content">
-                        <div class="panel-body">
-                            <h3>asdasdsa</h3>
 
-                            {{$i}}
-                        </div>
-                    </div>
-                @endforeach
-                @foreach($product as $i)
-                    <div class="panel panel-content">
-                        <div class="panel-body">
-                            <h3>asdasdsa</h3>
-
-                            {{$i}}
-                        </div>
-                    </div>
-                @endforeach
-                @foreach($product as $i)
-                    <div class="panel panel-content">
-                        <div class="panel-body">
-                            <h3>asdasdsa</h3>
-
-                            {{$i}}
-                        </div>
-                    </div>
-                @endforeach
-                @foreach($product as $i)
-                    <div class="panel panel-content">
-                        <div class="panel-body">
-                            <h3>asdasdsa</h3>
-
-                            {{$i}}
-                        </div>
-                    </div>
-                @endforeach
         </div>
+
+        <div class="col-md-9 pull-right" >
+            <div class="panel panel-body">
+                <div class="panel-body">
+                    <h1>Winkelwagen</h1>
+
+                    <div class="col-sm-12" style="margin-bottom: 30px;">
+                        <table class="table">
+                            <tr>
+                                <td>1x</td>
+                                <td>big burger special
+                                    <i class="fa fa-pencil" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                    <br>rund, Gemaakt met: tomaat,</td>
+                                <td>
+                                    <i class="fa fa-trash" aria-hidden="true" style="margin-right: 50px;font-size: 18px !important;"></i>
+                                    <i class="fa fa-minus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                    <i class="fa fa-plus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                </td>
+                                <td><span class="pull-right">&#8364; 10,00 <small class="muted">incl btw</small></span></td>
+                            </tr>
+                            <tr>
+                                <td>1x</td>
+                                <td>Tomate soep
+                                    <i class="fa fa-pencil" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                    <br>rund, tomaat, ui</td>
+                                <td>
+                                    <i class="fa fa-trash" aria-hidden="true" style="margin-right: 50px;font-size: 18px !important;"></i>
+                                    <i class="fa fa-minus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                    <i class="fa fa-plus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                </td>
+                                <td><span class="pull-right">&#8364; 3 <small class="muted">incl btw</small></span></td>
+                            </tr>
+                            <tr>
+                                <td>2x</td>
+                                <td>Spa Rood
+                                    {{--<i class="fa fa-pencil" aria-hidden="true" style="font-size: 18px !important;"></i>--}}
+                                    {{--<br>rund, tomaat, ui</td>--}}
+                                <td>
+                                    <i class="fa fa-trash" aria-hidden="true" style="margin-right: 50px;font-size: 18px !important;"></i>
+                                    <i class="fa fa-minus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                    <i class="fa fa-plus pull-right" aria-hidden="true" style="font-size: 18px !important;"></i>
+                                </td>
+                                <td><span class="pull-right">&#8364; 2 <small class="muted">incl btw</small></span></td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <div class="col-md-6">
+                        <style>
+                            table th{
+                                font-size: 20px !important;
+                            }
+                        </style>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th style="font-size: 20px;">Subtotal</th>
+                                    <td><b style="font-size: 20px;">&#8364; 15,00</b></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th style="font-size: 15px;">Btw <span></span></th>
+                                <td>&#8364; 3,15</td>
+                            </tr>
+                            {{--<tr>--}}
+                                {{--<th style="font-size: 15px;">Btw <span></span></th>--}}
+                                {{--<td>b</td>--}}
+                            {{--</tr>--}}
+                            </tbody>
+                        </table>
+                    </div>
+                    {{--<hr>--}}
+                    <a class="btn btn-danger btn-lg col-md-6" style="margin-bottom: 10px;">Annuleren</a>
+
+                    <a class="btn btn-success  btn-lg col-md-6">Betalen</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 @endsection
 
 @push('css')
     <style>
+        .menu-cate{
+            background-image: url('/image/banner.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            opacity: 0.8;
+            /*background: #000000;*/
+        }
         .filter{
 
         }
         .fix-filter {
             position: fixed;
             top: 10px;
-            width: 250px;
+            /*width: 250px;*/
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+            border-bottom: 1px solid #ccc;
+        }
+
+        li:last-child {
+            border: none;
+        }
+
+        li a {
+            text-decoration: none;
+            color: #000;
+            display: block;
+            /*width: 200px;*/
+
+            -webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+            -moz-transition: font-size 0.3s ease, background-color 0.3s ease;
+            -o-transition: font-size 0.3s ease, background-color 0.3s ease;
+            -ms-transition: font-size 0.3s ease, background-color 0.3s ease;
+            transition: font-size 0.3s ease, background-color 0.3s ease;
+        }
+
+        li a:hover {
+            font-size: 30px;
+            background: #f6f6f6;
         }
     </style>
 @endpush
 @push('js')
     <script>
 
+        $(document).ready(function (){
+            $("#cart").click(function (){
+//                $(window).load(function() {
+                $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+//                });
+            });
+        });
+
+//        $(window).scroll(function () {
+//            if ($(this).scrollTop() > 100) {
+//                $('.scrollToTop').fadeIn();
+//            } else {
+//                $('.scrollToTop').fadeOut();
+//            }
+//        });
 //        document.addEventListener('scroll', function (event) {
 //            console.log(event.target.id ===  $("#filter"));
 //
@@ -91,31 +270,31 @@ asdasd
 //                console.log('scrolling', event.target);
 //            }
 //        }, true /*Capture event*/);
-
-        $(document).ready(function() {
-            var wrap = $("#filter");
-
-//            console.log(wrap);
-
-//            $("#app").on('scroll', '#filter', function(){
-//                console.log('Event Fired');
+//
+//        $(document).ready(function() {
+//            var wrap = $("#filter");
+//
+////            console.log(wrap);
+//
+////            $("#app").on('scroll', '#filter', function(){
+////                console.log('Event Fired');
+////            });
+//
+//            $(window).scroll(function () {
+//                console.log('logging ' + $(this).scrollTop());
 //            });
-
-            $(window).scroll(function(e) {
-                console.log('logging' + this.scrollTop);
-            });
-            wrap.on("scroll", function(e) {
-
-                console.log(e);
-
-                if (this.scrollTop > 147) {
-                    wrap.addClass("fix-filter");
-                } else {
-                    wrap.removeClass("fix-filter");
-                }
-
-            });
-        });
+//            wrap.on("scroll", function(e) {
+//
+//                console.log(e);
+//
+//                if ($(this).scrollTop() > 147) {
+//                    wrap.addClass("fix-filter");
+//                } else {
+//                    wrap.removeClass("fix-filter");
+//                }
+//
+//            });
+//        });
 
     </script>
 @endpush
--- resources/views/layouts/kiosk.blade.php
+++ resources/views/layouts/kiosk.blade.php
@@ -16,7 +16,7 @@
     <title>Kiosk</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- Scripts -->
     <script>
@@ -36,10 +36,73 @@
             display: flex;
             align-items: center;
         }
+        body{
+            background: #d9d9d9;
+        }
+         input[type=text] {
+            width: 130px;
+            box-sizing: border-box;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            background-color: white;
+            background-image: url('https://www.w3schools.com/howto/searchicon.png');
+            background-position: 10px 15px;
+            background-repeat: no-repeat;
+            padding: 12px 20px 12px 40px;
+            -webkit-transition: width 0.4s ease-in-out;
+            transition: width 0.4s ease-in-out;
+        }
+
+         input[type=text]:focus {
+            width: 100%;
+        }
     </style>
 </head>
 <body>
-    <div id="app">
+
+    <nav class="navbar navbar-default navbar-fixed-top" style="background: #50A7DD; border: none;">
+        <div class="container">
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav" style="width: 50%;">
+                    {{--<form>--}}
+                    <input type="text" name="search" placeholder="Search.." style="margin: 5px 0px;">
+                    {{--</form>--}}
+                    {{--<li class="active" style="margin:0px auto">--}}
+                        {{--<a href="#" style="color: #FFFFFF; background: #52bdf4 !important;"><input id="search" class="form-control" name="search" placeholder="Search"></a>--}}
+                    {{--</li>--}}
+                    {{--<li><a href="#about">About</a></li>--}}
+                    {{--<li><a href="#contact">Contact</a></li>--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>--}}
+                        {{--<ul class="dropdown-menu">--}}
+                            {{--<li><a href="#">Action</a></li>--}}
+                            {{--<li><a href="#">Another action</a></li>--}}
+                            {{--<li><a href="#">Something else here</a></li>--}}
+                            {{--<li role="separator" class="divider"></li>--}}
+                            {{--<li class="dropdown-header">Nav header</li>--}}
+                            {{--<li><a href="#">Separated link</a></li>--}}
+                            {{--<li><a href="#">One more separated link</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active" >
+                        <a id="cart" href="#" style="color: #FFFFFF; background: #52bdf4 !important;">
+                            {{--<i class="fa fa-eur" aria-hidden="true" style="font-size: 37px;"> <span>15.00</span></i>--}}
+                            <b style="font-size: 20px; margin-top: -10px;">Winkelwagen</b>
+                            <i class="fa fa-shopping-cart" aria-hidden="true" style="font-size: 37px;"></i>
+                            <div style="background: black; border-radius: 50%; height: 20px; width: 20px; padding: 1px;position: absolute; margin-top: -40px; right: 5px;">
+                                <p class="text-center" style="color:#FFFFFF;"><b>4</b></p>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div id="app" style="margin-top: 100px;">
         <div class="vertical-center">
             @yield('content')
         </div>
